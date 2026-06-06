7a2588f845902632e0e0990117f8487750a74af8
Kristian Nissen <kristian.nissen@gmail.com>
2026-06-06 09:14:28 +0200

    remove Vercel stub files

--- api/handler.go
+++
@@ -1,6 +0,0 @@
-// This file has been intentionally removed.
-// Vercel support was dropped; the application runs as a standard Go HTTP server.
-// Delete this file and the api/ directory from the repository.
-//go:build ignore
-
-package handler
