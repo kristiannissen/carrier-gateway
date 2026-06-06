002c56adde2eaa934f4c9cf9c782b34e371725a3
Kristian Nissen <kristian.nissen@gmail.com>
2026-06-06 09:06:43 +0200

    Removing service point

--- internal/handler/service_points.go
+++
@@ -1,6 +0,0 @@
-// This file has been intentionally removed.
-// Service point support was dropped; e-commerce businesses implement that on their own.
-// Delete this file from the repository.
-//go:build ignore
-
-package handler
